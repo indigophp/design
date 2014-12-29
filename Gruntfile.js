88b9e8a941839dcedf148987047c13dd891f0399
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-12-29 15:41:04 +0100

    Full package redesign

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,208 +1,195 @@
 module.exports = function(grunt) {
-  "use strict";
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		path: {
+			dist: 'dist',
+			asset: 'assets',
+			icon: '<%= path.asset %>/icons',
+		},
+		clean: {
+			dist: '<%= path.dist %>',
+		},
+		less: {
+			dist: {
+				options: {
+					comporess: true,
+					sourceMap: true,
+					outputSourceFiles: true,
+					sourceMapURL: 'style.css.map',
+					sourceMapFilename: '<%= path.dist %>/<%= path.asset %>/css/style.css.map',
+					modifyVars: {
+						modernizrClass: '<%= modernizr.dist.extensibility.cssclassprefix %>',
+					},
+				},
+				files: {
+					'<%= path.dist %>/<%= path.asset %>/css/style.css': 'src/less/style.less',
+				},
+			},
+			minified: {
+				options: {
+					cleancss: true,
+					report: 'min',
+				},
+				files: {
+					'<%= path.dist %>/<%= path.asset %>/css/style.min.css': '<%= path.dist %>/<%= path.asset %>/css/style.css',
+				},
+			},
+		},
+		uglify: {
+			options: {
+				mangle: false,
+				report: 'min',
+			},
+			js: {
+				files: [{
+					expand: true,
+					cwd: 'src/js/',
+					src: '**/*.js',
+					dest: '<%= path.dist %>/<%= path.asset %>/js/',
+					ext: '.min.js',
+				}],
+			},
+			bootstrap_plugins: {
+				src: [
+					'bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js',
+					'bower_components/bootstrap-select/bootstrap-select.js',
+					'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+					'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
+				],
+				dest: '<%= path.dist %>/<%= path.asset %>/js/lib/bootstrap-plugins.min.js',
+			},
+		},
+		copy: {
+			assets: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src:['fonts/**', 'img/**', 'js/**'],
+						dest:'<%= path.dist %>/<%= path.asset %>/',
+					},
+				],
+			},
+			vendor: {
+				files: [
+					{
+						expand: true,
+						flatten: true,
+						src: [
+							'bower_components/jquery/dist/jquery.min.js',
+							'bower_components/bootstrap/dist/js/bootstrap.min.js',
+							'bower_components/html5shiv/dist/html5shiv.js',
+							'bower_components/respond/dest/respond.min.js',
+							'bower_components/datatables/media/js/jquery.dataTables.min.js',
+							'bower_components/moment/min/moment-with-langs.min.js',
+							'bower_components/bootbox/bootbox.js',
+						],
+						dest: '<%= path.dist %>/<%= path.asset %>/js/lib/',
+					},
+					{
+						expand: true,
+						flatten: true,
+						src: [
+							'bower_components/bootstrap/dist/fonts/*',
+							'bower_components/font-awesome/fonts/*',
+						],
+						dest: '<%= path.dist %>/<%= path.asset %>/fonts/',
+					},
+				],
+			},
+		},
+		processhtml: {
+			options: {
+				recursive: true,
+				process: true,
+				data: {
+					path: {
+						asset: '<%= path.asset %>',
+						icon: '<%= path.icon %>',
+					},
+					modernizrClass: '<%= modernizr.dist.extensibility.cssclassprefix %>',
+				},
+			},
+			dist: {
+				files:[
+					{
+						expand: true,
+						flatten: true,
+						src: 'src/html/*.html',
+						dest: '<%= path.dist %>/',
+					},
+				],
+			},
+			admin: {
+				files:[
+					{
+						expand: true,
+						flatten: true,
+						src: 'src/html/admin/*.html',
+						dest: '<%= path.dist %>/admin/',
+					},
+				],
+			},
+		},
+		jsbeautifier: {
+			files : ['<%= path.dist %>/**/*.html'],
+		},
+		modernizr: {
+			dist: {
+				devFile: 'bower_components/modernizr/modernizr.js',
+				outputFile: '<%= path.dist %>/<%= path.asset %>/js/lib/modernizr.min.js',
+				extra: {
+					shiv: false,
+					mq: true,
+					cssclasses: true,
+				},
+				extensibility: {
+					cssclassprefix: '',
+				},
+				uglify: true,
+				tests: [],
+				files: {
+					src: [
+						'<%= path.dist %>/<%= path.asset %>/css/*.css',
+						'<%= path.dist %>/<%= path.asset %>/js/*.js',
+					],
+				},
+			},
+		},
+		watch: {
+			options: {
+				livereload: false,
+			},
+			less: {
+				files: 'src/less/*.less',
+				tasks: 'less',
+			},
+			html: {
+				files: 'src/html/**/*.html',
+				tasks: 'processhtml',
+			},
+			js: {
+				files: 'src/js/*.js',
+				tasks: 'uglify',
+			},
+		},
+		connect: {
+			server: {
+				options: {
+					livereload: true,
+					port: 9000,
+					base: '<%= path.dist %>',
+				},
+			},
+		},
+	});
 
-  grunt.util.linefeed = '\n';
+	require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*!\n' +
-              ' * Indigo Theme v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-              ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-              ' */\n\n',
-
-    distPath: "dist",
-
-    clean: {
-      dist: ['<%= distPath %>']
-    },
-
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: false
-      },
-      bootstrap: {
-        src: [
-          'bower_components/bootstrap/dist/js/bootstrap.js',
-          'bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js',
-          'bower_components/bootstrap-select/bootstrap-select.js',
-          'bower_components/bootstrap-spinner/dist/bootstrap-spinner.js',
-          'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
-          'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
-          'js/bootstrap.js'
-        ],
-        dest: '<%= distPath %>/assets/js/lib/bootstrap.js'
-      }
-    },
-
-    uglify: {
-      options: {
-        banner: '<%= banner %>',
-        report: 'min'
-      },
-      bootstrap: {
-        src: ['<%= concat.bootstrap.dest %>'],
-        dest: '<%= distPath %>/assets/js/lib/bootstrap.min.js'
-      }
-    },
-
-    less: {
-      dist: {
-        options: {
-          strictMath: true,
-          sourceMap: true,
-          outputSourceFiles: true,
-          sourceMapURL: 'style.css.map',
-          sourceMapFilename: '<%= distPath %>/assets/css/style.css.map'
-        },
-        files: {
-          '<%= distPath %>/assets/css/style.css': 'less/style.less'
-        }
-      },
-      min: {
-        options: {
-          cleancss: true,
-          report: 'min'
-        },
-        files: {
-          '<%= distPath %>/assets/css/style.min.css': '<%= distPath %>/assets/css/style.css'
-        }
-      }
-    },
-
-    copy: {
-      fonts: {
-        expand: true,
-        flatten: true,
-        src: [
-          "fonts/*",
-          "bower_components/bootstrap/dist/fonts/*",
-          "bower_components/font-awesome/fonts/*"
-        ],
-        dest: '<%= distPath %>/assets/fonts/'
-      },
-      img: {
-        expand: true,
-        src: ["img/**/*"],
-        dest: '<%= distPath %>/assets/'
-      },
-      js: {
-        expand: true,
-        src: ["js/*", "!js/bootstrap.js"],
-        dest: '<%= distPath %>/assets/'
-      },
-      vendor_js: {
-        expand: true,
-        flatten: true,
-        src: [
-          'bower_components/jquery/dist/jquery.min.js',
-          'bower_components/jquery-migrate/jquery-migrate.min.js',
-          'bower_components/datatables/media/js/jquery.dataTables.js',
-          'bower_components/html5shiv/dist/html5shiv.js',
-          'bower_components/modernizr/modernizr.js',
-          'bower_components/moment/min/moment-with-langs.min.js',
-          'bower_components/respond/dest/respond.min.js',
-          'bower_components/bootbox/bootbox.js',
-        ],
-        dest: '<%= distPath %>/assets/js/lib/'
-      },
-      ckeditor: {
-        expand: true,
-        cwd: "bower_components/",
-        src: [
-          "ckeditor/*.js",
-          "ckeditor/contents.css",
-          "ckeditor/lang/**",
-          "ckeditor/plugins/**",
-          "ckeditor/adapters/jquery.js",
-          "ckeditor/skins/**",
-        ],
-        dest: '<%= distPath %>/assets/js/lib/'
-      },
-      json: {
-        expand: true,
-        cwd: "src/",
-        src: "admin/source.json",
-        dest: '<%= distPath %>/'
-      }
-    },
-
-    processhtml: {
-      options: {
-        recursive: true
-      },
-      dist: {
-        files:[
-          {
-            expand: true,
-            flatten: true,
-            src: ['src/*.html'],
-            dest: '<%= distPath %>/'
-          }
-        ]
-      },
-      admin: {
-        files:[
-          {
-            expand: true,
-            flatten: true,
-            src: ['src/admin/*.html'],
-            dest: '<%= distPath %>/admin/'
-          }
-        ]
-      }
-    },
-
-    jsbeautifier: {
-      files : '<%= distPath %>/*.html',
-    },
-
-    watch: {
-      options: { livereload: true },
-      less: {
-        files: 'less/**/*.less',
-        tasks: ['less']
-      },
-      html: {
-        files: 'src/**/*.html',
-        tasks: ['html']
-      }
-    },
-
-    connect: {
-      server: {
-        options: {
-          livereload: true,
-          port: 4000,
-          base: 'dist',
-          hostname: '*',
-        }
-      }
-    }
-  });
-
-  require('load-grunt-tasks')(grunt);
-
-  grunt.registerTask('js', ['concat', 'uglify']);
-
-  grunt.registerTask('css', ['less']);
-
-  grunt.registerTask('assets', ['css', 'js', 'copy']);
-
-  grunt.registerTask('html', ['processhtml', 'jsbeautifier']);
-
-  grunt.registerTask('dist', ['clean', 'assets', 'html']);
-
-  grunt.registerTask('default', ['connect', 'watch']);
-
-  grunt.registerTask('deploy', function() {
-    var path = grunt.option('path');
-
-    grunt.config('distPath', path);
-    grunt.config('clean.dist', path + '/assets');
-
-    grunt.task.run(['clean', 'assets']);
-  });
+	// Task definition
+	grunt.registerTask('assets', ['less', 'uglify', 'copy', 'modernizr']);
+	grunt.registerTask('html', ['processhtml', 'jsbeautifier']);
+	grunt.registerTask('dist', ['clean', 'assets', 'html']);
+	grunt.registerTask('work', ['connect', 'watch']);
+	grunt.registerTask('default', 'dist');
 };
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # IndigoPHP Design
 
-IndigoPHP site and admin design
+**IndigoPHP site and admin design.**
+
+This repository contains a plain html sitebuild of the default theme for indigophp framework. You can take a look at it: [http://design.indigophp.com](http://design.indigophp.com).
 
 
 ## License
 
-The MIT License (MIT). Please see [License File](https://github.com/indigophp/design/blob/develop/LICENSE) for more information.
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
